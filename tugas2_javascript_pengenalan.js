fc24fa2f2e79c618afdf17265236a31f7a847800
rahmathkd09 <rahmathkd09@gmail.com>
2020-09-18 10:44:54 +0700

    Add files via upload

---
+++ tugas2_javascript_pengenalan.js
@@ -0,0 +1,10 @@
+var nama="RAHMAT HIDAYAT"
+var nip=123455677
+var menikah=true
+var dataTeman=["fajar","dita","sukma"]
+var dataDiri={tinggi:170,berat:70, kulit:merah}
+console.log(nama)
+console.log(nip)
+console.log(menikah)
+console.log(dataTeman)
+console.log(dataDiri)
